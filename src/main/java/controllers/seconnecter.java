2aa65059f8fbb5edd6a6f1fbaca00026efbc76b5
aleaouf <alaeddine.aouf@esprit.tn>
2024-02-27 15:07:23 +0100

    first commit

--- src/main/java/controllers/seconnecter.java
+++ src/main/java/controllers/seconnecter.java
@@ -98,7 +98,7 @@ public class seconnecter {
 
 
     private void showAlert(String title, String content) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(content);
